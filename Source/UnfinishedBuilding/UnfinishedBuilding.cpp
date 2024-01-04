36fe2f92bfc52094a5a6ca415e0043572e7a94b8
Piang <974805475@qq.com>
2024-01-05 01:10:56 +0800

    init content

---
+++ Source/UnfinishedBuilding/UnfinishedBuilding.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnfinishedBuilding.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnfinishedBuilding, "UnfinishedBuilding" );
---
+++ Source/UnfinishedBuilding/UnfinishedBuildingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnfinishedBuildingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNFINISHEDBUILDING_API AUnfinishedBuildingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
